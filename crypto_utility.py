856ed3be41d13476259338b488fef593ef1d46b9
Brotherhood94 <alessandro.berti94@hotmail.it>
2020-10-28 00:57:36 +0100

    Refactored CryptoUtility

---
+++ crypto_utility.py
@@ -0,0 +1,52 @@
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import json
+
+if __name__ == '__main__':
+    encrypt_and_store()
+
+def get_key(password):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(password)
+    return base64.urlsafe_b64encode(digest.finalize())
+
+def encrypt_and_store():
+    password = None
+    token = None
+    file_name = None
+
+    try: 
+        password = input("Enter Encryption Password: ")
+        token = input("What to encrypt: ").encode()
+        file_name = input("Where to store: ")
+    except EOFError:
+        print("EOFError")
+
+    fernet = Fernet(get_key(password.encode('utf-8')))
+    encrypted = fernet.encrypt(token)
+
+    #Store to file
+    f = open(file_name, 'wb')
+    f.write(encrypted)
+    f.close
+
+
+
+def decrypt_file(file_name):
+    password = None
+    try: 
+        password = input("Enter Decryption Password: ")
+    except EOFError:
+        print("EOFError")
+
+    fernet = Fernet(get_key(password.encode('utf-8')))
+
+    f = open(file_name, 'rb')
+    encrypted_token = f.read()
+    f.close()
+
+    decrypted = fernet.decrypt(encrypted_token)
+    return decrypted.decode()
+
--- deliver.py
+++ deliver.py
@@ -1,38 +1,12 @@
 import base64
-
+import crypto_utility
 from PPfacebook import PP_facebook
 
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
-
-def get_key(password):
-    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    digest.update(password)
-    return base64.urlsafe_b64encode(digest.finalize())
-
-def decrypt_file(password, file_name):
-    fernet = Fernet(get_key(password.encode('utf-8')))
-    f = open(file_name, 'rb')
-    encrypted_token = f.read()
-    f.close()
-    d = fernet.decrypt(encrypted_token)
-    return d.decode()
 
 def main():
-    password = None
-    try: 
-        password = input("Enter Decryption Password: ")
-    except EOFError:
-        print("EOFError")
-
-    PP_facebook_token = decrypt_file(password, "./access_tokens/Facebook_token.enc")
+    PP_facebook_token = crypto_utility.decrypt_file("./access_tokens/Facebook_token.enc")
     pp_facebook = PP_facebook(PP_facebook_token)
     pp_facebook.get_message('836872193518697')
 
 
-main()
-    
-
-
-
+main() 
